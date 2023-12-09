fd816b70b523ba5c170d4d846f29c49c72840101
Martins Veiskats <martins.veiskats@gmail.com>
2023-12-09 12:37:18 +0200

    Solution for day 2

---
+++ 02-1.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main() {
+    let result: usize = io::stdin().lines().filter_map(|line| {
+        let line = line.unwrap();
+        let (game_id, cubes) = line.split_once(": ").unwrap();
+        let game_id = game_id[5..].parse::<usize>().unwrap();
+
+        if cubes.split("; ").all(|handful| {
+            handful.split(", ").all(|cube| {
+                let (number, color) = cube.split_once(' ').unwrap();
+                let number = number.parse::<usize>().unwrap();
+                (color == "red" && number <= 12) ||
+                    (color == "green" && number <= 13) ||
+                    (color == "blue" && number <= 14)
+            })
+        }) {
+            Some(game_id)
+        } else {
+            None
+        }
+    }).sum();
+
+    println!("{result}");
+}
---
+++ 02-2.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::cmp;
+
+fn main() {
+    let result: usize = io::stdin().lines().map(|line| {
+        let line = line.unwrap();
+        let (_game_id, cubes) = line.split_once(": ").unwrap();
+
+        let mut min_red = 0;
+        let mut min_green = 0;
+        let mut min_blue = 0;
+
+        cubes.split("; ").for_each(|handful| {
+            handful.split(", ").for_each(|cube| {
+                let (number, color) = cube.split_once(' ').unwrap();
+                let number = number.parse::<usize>().unwrap();
+
+                match color {
+                    "red" => min_red = cmp::max(min_red, number),
+                    "green" => min_green = cmp::max(min_green, number),
+                    "blue" => min_blue = cmp::max(min_blue, number),
+                    _ => panic!()
+                }
+            })
+        });
+
+        min_red * min_green * min_blue
+    }).sum();
+
+    println!("{result}");
+}
